390e8a36effe6529613071ccccebc1b4ffd5265c
nikolcar <mitic.nikolca94@gmail.com>
2017-10-28 13:51:18 +0200

    DateTime format + js refactoring

--- LocateParking/LocateParking/Scripts/SignalR.js
+++ LocateParking/LocateParking/Scripts/SignalR.js
@@ -1,25 +1,33 @@
-﻿$.connection.hub.start()
-    .done(function () {
-        console.log("IT WORKED!")
-        $.connection.myHub.server.addRows();
-    })
-    .fail(function () { alert("ERROR!") });
+﻿
+(function ()
+{
+    $.connection.hub.start()
+        .done(function ()
+        {
+            $.connection.myHub.server.addRows();
+        })
+        .fail(function ()
+        {
+            alert("SignalR connection ERROR!");
+        });
 
-$.connection.myHub.client.add = function (data) {
-    var table = document.getElementById("myTable");
+    $.connection.myHub.client.add = function (data)
+    {
+        var table = document.getElementById("myTable");
 
-    var rowCount = table.rows.length;
-    var row = table.insertRow(rowCount);
+        var rowCount = table.rows.length;
+        var row = table.insertRow(rowCount);
 
-    var number = row.insertCell(0);
-    var date = row.insertCell(1);
-    var user = row.insertCell(2);
-    var parking = row.insertCell(3);
-    var type = row.insertCell(4);
+        var number = row.insertCell(0);
+        var date = row.insertCell(1);
+        var user = row.insertCell(2);
+        var parking = row.insertCell(3);
+        var type = row.insertCell(4);
 
-    number.innerHTML = rowCount;
-    date.innerHTML = data.dateTime;
-    user.innerHTML = data.userName;
-    parking.innerHTML = data.parkingName;
-    type.innerHTML = data.parkingType;
-}
+        number.innerHTML = rowCount;
+        date.innerHTML = new Date(data.dateTime).toLocaleString();
+        user.innerHTML = data.userName;
+        parking.innerHTML = data.parkingName;
+        type.innerHTML = data.parkingType;
+    }
+})()
